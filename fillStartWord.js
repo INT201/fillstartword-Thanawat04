7f7e294ad7e785762de271f443d439743b79e3ee
Thanawat Chen <thanawat.th@mail.kmutt.ac.th>
2022-09-09 18:41:35 +0700

    v2

--- fillStartWord.js
+++ fillStartWord.js
@@ -3,12 +3,10 @@ const { template } = require('@babel/core')
 function fillStartWord(startWord, word) {
     if (word===null||word===undefined)
         return undefined
-    else if (word === 'Hello World')
-        return  startWord+word
-    else if (word === 'beginner')
-        return startWord+word
     else if (word.includes(startWord))
         return word
+    else
+        return  startWord+word
 }
 
 module.exports = fillStartWord
